f77d2b9006108a233513306e4d6f0a1d6a02cdac
alireza <alireza.ksv@gmail.com>
2022-01-09 22:19:20 +0330

    added chat class

---
+++ src/Chat.php
@@ -0,0 +1,29 @@
+<?php
+namespace Chat;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+class Chat implements MessageComponentInterface {
+    protected $clients;
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);        
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send("(Friend $from->resourceId) : ". $msg);
+            }
+        }
+    }
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
